797fed2c77934f4519974ab33dd139f9c4db8b87
KamiKemiKimi <40531928+KamiKemiKimi@users.noreply.github.com>
2023-02-28 13:34:49 -0800

    First Commit

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,44 @@
+import React, { Fragment, useState, useEffect } from 'react';
+import { toast } from 'react-toastify';
+
+const Dashboard = ({ setAuth }) => {
+
+  const [name, setName] = useState("");
+
+  async function getName() {
+    try {
+      const response = await fetch("http://localhost:5000/dashboard/", {
+        method: "GET",
+        headers: { token: localStorage.token }
+      });
+
+      const parseRes = await response.json();
+
+      setName(parseRes.user_name);
+
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+
+  const logout = (e) => {
+    e.preventDefault();
+    localStorage.removeItem('token');
+    setAuth(false);
+    toast.success("Logged out successfully!");
+  }
+
+  useEffect(() => {
+    getName()
+  }, [])
+
+  return (
+    <Fragment>
+      <h1>Dashboard - Welcome {name}</h1>
+      <button className="btn btn-primary" onClick={e => logout(e)}>Logout</button>
+
+    </Fragment>
+  );
+};
+
+export default Dashboard;
