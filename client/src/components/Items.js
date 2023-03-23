5a9fb82a8407a21b1d914f1ce93dfc1f7bde5253
KamiKemiKimi <alexkim280@gmail.com>
2023-03-22 21:24:48 -0700

    Modal In Progress...

--- client/src/components/Items.js
+++ client/src/components/Items.js
@@ -1,6 +1,8 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import Nav from './Nav';
 import './nav.css';
+import EditItem from "./EditItem";
+
 
 const Items = () => {
 
@@ -21,6 +23,20 @@ const Items = () => {
         };
     }
 
+    const deleteItem = async id => {
+        try {
+            const deleteItem = await fetch(`http://localhost:5000/dashboard/item/${id}`, {
+                method: "DELETE",
+                headers: { token: localStorage.token }
+            });
+
+            setItems(items.filter(item => item.item_id !== id));
+
+        } catch (err) {
+            console.error(err.message);
+        }
+    };
+
     useEffect(() => {
         getItems();
     }, []);
@@ -29,24 +45,33 @@ const Items = () => {
         <Fragment>
             <Nav />
             <h1>Items</h1>
-            <table className="table table-striped">
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Description</th>
-                        <th>Price</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {items && items.map((item) => (
-                        <tr key={item.id}>
-                            <td>{item.name}</td>
-                            <td>{item.description}</td>
-                            <td>{item.price}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+            <div style={{ display: 'flex', justifyContent: 'center', backgroundColor: 'white', padding: '20px' }}>
+                <div style={{ maxWidth: '800px' }}>
+                    <table className="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Description</th>
+                                <th>Price</th>
+                                <th></th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {items && items.map((item) => (
+                                <tr key={item.id}>
+                                    <td>{item.name}</td>
+                                    <td>{item.description}</td>
+                                    <td>{item.price}</td>
+                                    <td><EditItem item={item} /></td>
+                                    <td><button className="btn btn-danger" onClick={() => deleteItem(item.item_id)}>Delete</button></td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
         </Fragment>
     );
 };
