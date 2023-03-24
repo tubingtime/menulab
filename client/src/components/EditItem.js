755e1fb991978906a6d1b5267f1b30f415f5ff51
KamiKemiKimi <alexkim280@gmail.com>
2023-03-23 21:20:17 -0700

    Yep.

--- client/src/components/EditItem.js
+++ client/src/components/EditItem.js
@@ -1,4 +1,8 @@
 import React, { Fragment, useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 const EditItem = ({ item }) => {
 
