382fb02445870fe989f8e502869715c61c67fd8b
KamiKemiKimi <alexkim280@gmail.com>
2023-03-23 19:54:19 -0700

    delete hamburger menu

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,12 +4,10 @@ import { Link } from 'react-router-dom';
 
 function Home(props) {
     return (
+        
         <div id="app-container" className="home">
             <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top" style={{ boxShadow: "0px 0px 8px #888888" }}>
                 <div className="container">
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav ml-auto">
                             <li className="nav-item">
@@ -23,6 +21,7 @@ function Home(props) {
             <h1 class="home-title">
                 <span>MenuLab</span>
             </h1>
+
         </div>
 
     );
