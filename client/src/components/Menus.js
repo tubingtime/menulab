cb9a5c49a4aa72f3b8c26d22d363423e41f8c406
KamiKemiKimi <alexkim280@gmail.com>
2023-03-24 14:34:07 -0700

    Added 'add menu' and 'delete menu' functionality

--- client/src/components/Menus.js
+++ client/src/components/Menus.js
@@ -5,6 +5,20 @@ import Nav from './Nav.js';
 const Menus = () => {
     const [menus, setMenus] = useState("");
 
+    const deleteMenu = async id => {
+        try {
+            const deleteItem = await fetch(`http://localhost:5000/dashboard/menus/${id}`, {
+                method: "DELETE",
+                headers: { token: localStorage.token }
+            });
+
+            setMenus(menus.filter(menu => menu.menu_id !== id));
+
+        } catch (err) {
+            console.error(err.message);
+        }
+    };
+
     const getMenus = async () => {
         try {
             const response = await fetch("http://localhost:5000/dashboard/menus", {
@@ -20,6 +34,29 @@ const Menus = () => {
         };
     }
 
+    const [name, setName] = useState("");
+
+    const onSubmitForm = async (e) => {
+        e.preventDefault();
+        try {
+            console.log("onSubmit");
+            const body = { name };
+
+            /* fetch() makes a GET request by default. */
+            console.log(JSON.stringify(body));
+            const response = await fetch("http://localhost:5000/dashboard/menus", {
+                method: "POST",
+                headers: { "Content-Type": "application/json", token: localStorage.token },
+                body: JSON.stringify(body)
+            });
+            console.log(response);
+
+            window.location = "/menus";
+        } catch (err) {
+            console.error(err.message);
+        }
+    };
+
     useEffect(() => {
         getMenus();
     }, []);
@@ -29,6 +66,24 @@ const Menus = () => {
         <Fragment>
             <Nav />
             <h1>Menus</h1>
+            <form className="mt-2" onSubmit={onSubmitForm}>
+                <div className="row">
+                    <div className="col">
+                        <input
+                            type="text"
+                            name="name"
+                            placeholder="name"
+                            required
+                            className="form-control"
+                            value={name}
+                            onChange={e => setName(e.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <button className="btn btn-success">Add</button>
+                    </div>
+                </div>
+            </form>
             <div className="row row-cols-1 row-cols-md-2 g-4">
                 {menus && menus.map((menu, i) => (
                     <div className="column">
@@ -39,7 +94,7 @@ const Menus = () => {
                             <div className="card-body">
                                 <div className="d-grid gap-2 d-md-flex justify-content-md-end">
                                     <a href="#" className="btn btn-outline-info">Edit</a>
-                                    <a href="#" className="btn btn-outline-danger">Delete</a>
+                                    <button className="btn btn-danger" onClick={() => deleteMenu(menu.menu_id)}>Delete</button>
                                 </div>
                             </div>
                         </div>
