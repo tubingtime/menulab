5f4306a75a6919819d24f3213690aecf0dfd8f44
tubingtime <tdelaveaga@dons.usfca.edu>
2023-03-26 21:49:04 -0700

    unprotect home page

--- client-next/src/middleware.tsx
+++ client-next/src/middleware.tsx
@@ -1,3 +1,6 @@
 // This middleware protects every page inside of this directory
 // More Details: https://next-auth.js.org/configuration/nextjs#middleware
 export { default } from "next-auth/middleware"
+
+
+export const config = { matcher: ["/dashboard:path"] }
