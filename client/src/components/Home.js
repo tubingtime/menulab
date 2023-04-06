88d2292e5cb06c35a938f60236da173979e5fa91
hatsh3p <sarahamarragy@gmail.com>
2023-04-06 09:23:22 -0700

    Revert "Removed /client folder."
    This reverts commit dcdf8aa8976421b506aae72d39febaae7d2740b0.

---
+++ client/src/components/Home.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './home.css';
+import { Link } from 'react-router-dom';
+
+function Home(props) {
+    return (
+        
+        <div id="app-container" className="home">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top" style={{ boxShadow: "0px 0px 8px #888888" }}>
+                <div className="container">
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item">
+                                <Link to="/login" className="btn btn-primary">Login</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+            <h1 class="home-title">
+                <span>MenuLab</span>
+            </h1>
+
+        </div>
+
+    );
+}
+
+export default Home;
---
+++ client/src/components/Items.js
@@ -0,0 +1,143 @@
+import React, { Fragment, useState, useEffect } from 'react';
+import Nav from './Nav';
+import './nav.css';
+import EditItem from "./EditItem";
+
+
+const Items = () => {
+
+    const [items, setItems] = useState([]);
+
+    const getItems = async () => {
+        try {
+            const response = await fetch("http://localhost:5000/dashboard/items", {
+                method: "GET",
+                headers: { token: localStorage.token }
+            });
+
+            const jsonData = await response.json();
+            console.log(jsonData);
+            setItems(jsonData);
+        } catch (err) {
+            console.error(err.message);
+        };
+    }
+
+    const deleteItem = async id => {
+        try {
+            const deleteItem = await fetch(`http://localhost:5000/dashboard/item/${id}`, {
+                method: "DELETE",
+                headers: { token: localStorage.token }
+            });
+
+            setItems(items.filter(item => item.item_id !== id));
+
+        } catch (err) {
+            console.error(err.message);
+        }
+    };
+
+    const [inputs, setInputs] = useState({
+        name: '',
+        description: '',
+        price: ''
+    });
+    const { name, description, price } = inputs;
+    const onChange = e => {
+        setInputs({ ...inputs, [e.target.name]: e.target.value })
+    };
+
+    const onSubmitForm2 = async (e) => {
+        e.preventDefault();
+        try {
+            console.log("onSubmit");
+            const body = { name, description, price };
+            /* fetch() makes a GET request by default. */
+            console.log(JSON.stringify(body));
+            const response = await fetch("http://localhost:5000/dashboard/item", {
+                method: "POST",
+                headers: { "Content-Type": "application/json", token: localStorage.token },
+                body: JSON.stringify(body)
+            });
+            console.log(response);
+
+            window.location.reload();
+        } catch (err) {
+            console.error(err.message);
+        }
+    };
+
+    useEffect(() => {
+        getItems();
+    }, []);
+
+    return (
+        <Fragment>
+            <Nav />
+            <h1>Items</h1>
+            <form className="mt-2" onSubmit={onSubmitForm2}>
+                <div className="row">
+                    <div className="col">
+                        <input
+                            type="text"
+                            name="name"
+                            placeholder="name"
+                            required
+                            className="form-control"
+                            value={name}
+                            onChange={e => onChange(e)}
+                        />
+                        <input
+                            type="text"
+                            name="description"
+                            placeholder="description"
+                            className="form-control"
+                            value={description}
+                            onChange={e => onChange(e)}
+                        />
+                        <input
+                            type="text"
+                            name="price"
+                            placeholder="price"
+                            className="form-control"
+                            value={price}
+                            onChange={e => onChange(e)}
+                        />
+                    </div>
+                    <div>
+                        <button className="btn btn-success">Add</button>
+                    </div>
+                </div>
+            </form>
+            <div style={{ display: 'flex', justifyContent: 'center', backgroundColor: 'white', padding: '20px' }}>
+                <div style={{ maxWidth: '800px' }}>
+                    <table className="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Description</th>
+                                <th>Price</th>
+                                <th></th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {items.map((item, i) => (
+                                <tr key={i}>
+                                    <td>{item.name}</td>
+                                    <td>{item.description}</td>
+                                    <td>{item.price}</td>
+                                    <td><EditItem item={item} /></td>
+                                    <td><button className="btn btn-danger" onClick={() => deleteItem(item.item_id)}>Delete</button></td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        </Fragment>
+    );
+};
+
+export default Items;
