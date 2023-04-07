b21bf862c32a33a6636fc367ab7758f3d28d9b90
hatsh3p <sarahamarragy@gmail.com>
2023-04-06 19:32:23 -0700

    Used AddItem component in items page.

--- client-next/src/app/dashboard/items/page.tsx
+++ client-next/src/app/dashboard/items/page.tsx
@@ -3,8 +3,8 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import Nav from '@/components/Nav';
 import { useToken } from '@/lib/SessionManagement';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
 import DisplayItems from '@/components/DisplayItems';
+import AddItem from '@/components/AddItem';
 
 
 const Items = () => {
@@ -30,36 +30,6 @@ const Items = () => {
             console.error(err.message);
         };
     }
-
-    const [inputs, setInputs] = useState({
-        name: '',
-        description: '',
-        price: ''
-    });
-    const { name, description, price } = inputs;
-    const onChange = e => {
-        setInputs({ ...inputs, [e.target.name]: e.target.value })
-    };
-
-    const onSubmitForm2 = async (e) => {
-        e.preventDefault();
-        try {
-            console.log("onSubmit");
-            const body = { name, description, price };
-
-            /* fetch() makes a GET request by default. */
-            console.log(JSON.stringify(body));
-            const response = await fetch("http://localhost:5000/dashboard/item", {
-                method: "POST",
-                headers: { "Content-Type": "application/json", token: jwtToken },
-                body: JSON.stringify(body)
-            });
-            console.log(response);
-            window.location.reload();
-        } catch (err: any) {
-            console.error(err.message);
-        }
-    };
 
     useEffect(() => {
         getItems();
@@ -99,52 +69,7 @@ const Items = () => {
             </section>
             <section>
                 <h2>Add an Item</h2>
-                <form className="mt-2" onSubmit={onSubmitForm2}>
-                    <div className="row">
-
-                        <div className="col">
-                            <label>Name</label>
-                            <input
-                                type="text"
-                                name="name"
-                                placeholder="Enter item name."
-                                required
-                                className="form-control"
-                                value={name}
-                                onChange={e => onChange(e)}
-                            />
-                        </div>
-
-                        <div className="col">
-                            <label>Price</label>
-                            <input
-                                type="text"
-                                name="price"
-                                placeholder="Enter item price."
-                                className="form-control"
-                                value={price}
-                                onChange={e => onChange(e)}
-                            />
-                        </div>
-                    </div>
-
-                    <div className="row">
-                        <div className="col">
-                            <label>Description</label>
-                            <input
-                                type="text"
-                                name="description"
-                                placeholder="Optional: Add an item description."
-                                className="form-control"
-                                value={description}
-                                onChange={e => onChange(e)}
-                            />
-                        </div>
-                    </div>
-                    <div>
-                        <button className="btn btn-primary">Add</button>
-                    </div>
-                </form>
+                <AddItem />
             </section>
             <section>
                 <h2>Items</h2>
--- client-next/src/components/AddItem.tsx
+++ client-next/src/components/AddItem.tsx
@@ -1,6 +1,5 @@
 import React, { Fragment, useState } from "react";
 import { useToken } from "@/lib/SessionManagement";
-import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 const AddItem = () => {
 
