0b44f0658189f45b8076780cf624ed2a91d51d96
hatsh3p <sarahamarragy@gmail.com>
2023-04-07 14:32:33 -0700

    Implemented sections. Needs cleanup. Some buttons/functions not working.

--- client-next/src/app/dashboard/menus/listitems/page.tsx
+++ client-next/src/app/dashboard/menus/listitems/page.tsx
@@ -6,6 +6,8 @@ import { useSearchParams } from 'next/navigation'
 import { useToken } from '@/lib/SessionManagement';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import AddItem from '@/components/AddItem2';
+import DisplayMenuItems from '@/components/DisplayMenuItems';
+import DisplaySections from '@/components/DisplaySections';
 
 const ListItems = () => {
 
@@ -14,6 +16,29 @@ const ListItems = () => {
     const searchParams = useSearchParams();
     const menu_id = searchParams?.get('menu_id');
     const [menuName, setMenuName] = useState("");
+
+    // Get all items in a menu //
+    const [items, setItems] = useState<any[]>([]);
+    const getItems = async (menu_id) => {
+        try {
+            const response = await fetch(`http://localhost:5000/dashboard/menus/${menu_id}`, {
+                method: "GET",
+                headers: { token: jwtToken }
+            });
+
+            const jsonData = await response.json();
+            console.log(response);
+            // Sort the array by the 'name' field in ascending order
+            const sortedData = jsonData.sort((a, b) => a.name.localeCompare(b.name));
+
+            setItems(sortedData);
+        } catch (err: any) {
+            console.error(err.message);
+        };
+    }
+    useEffect(() => {
+        getItems(menu_id);
+    }, []);
 
     return (
         <Fragment>
@@ -24,6 +49,14 @@ const ListItems = () => {
             </section>
             <section>
                 <AddItem menu_id={menu_id} />
+            </section>
+            <section>
+                {/* For each Section */}
+                {/* Display Section Name */}
+                <DisplaySections menu_id={menu_id} />
+            </section>
+            <section>
+                <DisplayMenuItems items={items} />
             </section>
         </Fragment>
     );
