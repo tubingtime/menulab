b72d17d01bdabecce410527304a6aa13701cc544
hatsh3p <sarahamarragy@gmail.com>
2023-04-06 22:16:50 -0700

    Created a second AddItems component that works for both scenarios.

--- client-next/src/components/AddItem.tsx
+++ client-next/src/components/AddItem.tsx
@@ -1,5 +1,6 @@
 import React, { Fragment, useState } from "react";
 import { useToken } from "@/lib/SessionManagement";
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 const AddItem = () => {
 
