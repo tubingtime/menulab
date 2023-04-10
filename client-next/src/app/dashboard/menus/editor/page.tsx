c8f6a2642b9b09333ab2ca36b5d704c75ab7b1da
hatsh3p <sarahamarragy@gmail.com>
2023-04-10 11:26:08 -0700

    Cleaned up editor/page.

--- client-next/src/app/dashboard/menus/editor/page.tsx
+++ client-next/src/app/dashboard/menus/editor/page.tsx
@@ -1,25 +1,21 @@
 "use client"
-
 import React, { Fragment, useState, useEffect } from 'react';
 import Nav from '@/components/Nav';
 import { useSearchParams } from 'next/navigation'
 import { useToken } from '@/lib/SessionManagement';
 import AddItem from '@/components/AddItem';
-import AddSection from '@/components/AddSection'
 import DisplayMenuItems from '@/components/DisplayMenuItems';
 import DisplaySections from '@/components/DisplaySections';
 import "bootstrap/js/dist/dropdown"
 
 const ListItems = () => {
-
     const jwtToken = useToken();
-
     const searchParams = useSearchParams();
     const menu_id = searchParams?.get('menu_id');
     const [menuName, setMenuName] = useState("");
 
-    // Get all items in a menu //
     const [items, setItems] = useState<any[]>([]);
+
     const getItems = async (menu_id) => {
         try {
             const response = await fetch(`http://localhost:5000/dashboard/menus/${menu_id}`, {
@@ -28,15 +24,13 @@ const ListItems = () => {
             });
 
             const jsonData = await response.json();
-            // Sort the array by the 'name' field in ascending order
             const sortedData = jsonData.sort((a, b) => a.name.localeCompare(b.name));
-
             setItems(sortedData);
         } catch (err: any) {
             console.error(err.message);
         };
     }
-    
+
     async function getMenuName(menu_id) {
         try {
             const response = await fetch(`http://localhost:5000/dashboard/menus/name/${menu_id}`, {
@@ -45,7 +39,6 @@ const ListItems = () => {
             });
 
             const jsonData = await response.json();
-
             setMenuName(jsonData);
         } catch (err: any) {
             console.error(err.message);
@@ -64,12 +57,10 @@ const ListItems = () => {
                 <h1>{menuName}</h1>
             </section>
             <section>
+                <h2>Add Item</h2>
                 <AddItem menu_id={menu_id} />
             </section>
             <section>
-                {/* For each Section */}
-                {/* Display Section Name */}
-
                 <DisplaySections menu_id={menu_id} />
             </section>
             <section>
@@ -79,6 +70,5 @@ const ListItems = () => {
         </Fragment>
     );
 };
-
 
 export default ListItems;
