b356a2b8b9cc397703b3ff67504d62b1544cb239
hatsh3p <sarahamarragy@gmail.com>
2023-04-15 12:00:09 -0700

    Made Add Menu button in line with header and made button smaller.

--- client-next/src/app/dashboard/menus/page.tsx
+++ client-next/src/app/dashboard/menus/page.tsx
@@ -13,7 +13,23 @@ import DeleteMenu from '@/components/DeleteMenu';
 const Menus = () => {
     const jwtToken = useToken();
     const [menus, setMenus] = useState<any[]>([]);
+    const [name, setName] = useState("");
 
+    const onSubmitForm = async (e) => {
+        e.preventDefault();
+        try {
+            const body = { name };
+            const response = await fetch("http://localhost:5000/dashboard/menus", {
+                method: "POST",
+                headers: { "Content-Type": "application/json", token: jwtToken },
+                body: JSON.stringify(body)
+            });
+
+            getMenus();
+        } catch (err: any) {
+            console.error(err.message);
+        }
+    };
     const getMenus = async () => {
         try {
             const response = await fetch("http://localhost:5000/dashboard/menus", {
@@ -36,15 +52,12 @@ const Menus = () => {
         <Fragment>
             <Nav />
             <section>
-                <h1 className="display">Menus</h1>
+                <h1 style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                    <span>Menus</span>
+                    <AddMenu />
+                </h1>
             </section>
-
             <section>
-                <AddMenu />
-            </section>
-
-            <section>
-                <h2>Menus</h2>
                 <div className="row row-cols-1 row-cols-md-2 g-4">
                     {menus && menus.map((menu, i) => (
                         <div className="column" key={i}>
